4d7911116e184f6aefbe0413d00add4350bdc601
Danny Lin <danny@kdrag0n.dev>
2020-11-01 21:47:23 +0600

    platina: Use EXPENSIVE_RENDERING power hint for blurs
    We need to boost the GPU for smooth high-radius blurs as blurring is an
    expensive rendering job.
    
    Change-Id: Ia3968313601d2b04ce50c01ee61dc9944ee26187

--- properties.mk
+++ properties.mk
@@ -159,6 +159,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.max_virtual_display_dimension=4096 \
     ro.surface_flinger.protected_contents=true \
     ro.vendor.display.cabl=0 \
+    ro.sf.blurs_are_expensive=1 \
     ro.surface_flinger.supports_background_blur=1 \
     vendor.display.disable_skip_validate=1 \
     vendor.display.enable_default_color_mode=1 \
