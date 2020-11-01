787adde934c692d7814b69fb3868fb3e223427bd
Danny Lin <danny@kdrag0n.dev>
2020-11-01 21:42:59 +0600

    platina: Enable compositor background blurring
    This allows us to create a "glass blur" look throughout the system for
    improved visuals.
    
    Change-Id: I77bb13a5711b05b43acf25e12abd9a2f5cd2502d

--- properties.mk
+++ properties.mk
@@ -159,6 +159,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.max_virtual_display_dimension=4096 \
     ro.surface_flinger.protected_contents=true \
     ro.vendor.display.cabl=0 \
+    ro.surface_flinger.supports_background_blur=1 \
     vendor.display.disable_skip_validate=1 \
     vendor.display.enable_default_color_mode=1 \
     vendor.gralloc.enable_fb_ubwc=1
