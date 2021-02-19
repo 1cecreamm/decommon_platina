979acb04dcd03c30be14c98f45b1eeb96dd01c20
1cecreamm <teyengkuroba@gmail.com>
2021-02-19 16:01:39 +0700

    platina: adjust props & bringup dotos

--- dot_platina.mk
+++ dot_platina.mk
@@ -19,8 +19,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common PixelPlusUI stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common DotOs stuff
+$(call inherit-product, vendor/dot/config/common.mk)
 
 # Inherit from platina device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -28,19 +28,12 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := platina
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := aosp_platina
+PRODUCT_NAME := dot_platina
 PRODUCT_MODEL := Mi 8 Lite
 
 # PixelExperience Stuffs
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
-
-# OFFICAL STUFF
-CUSTOM_BUILD_TYPE=OFFICIAL
-TARGET_SUPPORTS_GOOGLE_RECORDER := false
-TARGET_INCLUDE_STOCK_ACORE := false
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
-TARGET_FACE_UNLOCK_SUPPORTED = true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
