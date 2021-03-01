a1e6a7b5105070452735b3af6c092c0fdfd0654a
Manish4586 <manish.n.manish45@gmail.com>
2021-03-01 21:09:34 +0530

    platina: audio: Switch to audio hal v4
    Signed-off-by: Hanif Ardhani <hanif.ardhani98@gmail.com>

--- device.mk
+++ device.mk
@@ -39,8 +39,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@6.0-impl:32 \
-    android.hardware.audio.effect@6.0-impl:32 \
+    android.hardware.audio@4.0-impl \
+    android.hardware.audio.effect@4.0-impl \
+    android.hardware.audio.effect@2.0-service \
     android.hardware.audio.service \
     audio.a2dp.default \
     audio.primary.sdm660 \
